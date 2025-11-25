92e53f2a78e04dfdce3ac97bbca696a947f83c5f
PabloAgulloMarti <147489182+PabloAgulloMarti@users.noreply.github.com>
2025-11-25 11:42:03 +0000

    sheet 4 Excercise 1a)
    done

--- .ccls-cache/@home@runner@workspace/likelihood.cc
+++ .ccls-cache/@home@runner@workspace/likelihood.cc
@@ -1,20 +1,36 @@
-#include <iostream>
+#include <cmath>
 #include <fstream>
+#include <iostream>
 #include <vector>
-
+using namespace std;
 
 double poisson(double mu, int k) {
-    return 0;
+  return pow(mu, k) * exp(-mu) / tgamma(k + 1);
+}
+
+double prob(const vector<int> &daten, double mu) {
+  double L = 1.0;
+  for (int k : daten) {
+    L *= poisson(mu, k);
+  }
+  return L;
 }
 
 int main() {
-    using namespace std;
+  vector<int> daten;
 
+  ifstream fin("datensumme.txt");
+  int zahl;
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  while (fin >> zahl) {
+    daten.push_back(zahl);
+  }
+  fin.close();
+
+  double mu = 3.11538; // sample mean
+  double L = prob(daten, mu);
+
+  cout << "Likelihood L(mu) = " << L << endl;
+
+  return 0;
 }
